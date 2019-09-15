de744b731639b019763d0f7c4b327c14133a6268
Julian Suggate <julian.suggate@gmail.com>
2019-09-15 15:45:40 +1200

    Add Tailwindcss and basic styles, Phx ~> 1.4.10

--- reflect_umbrella/apps/reflect_web/lib/reflect_web/views/view_helpers.ex
+++ reflect_umbrella/apps/reflect_web/lib/reflect_web/views/view_helpers.ex
@@ -4,6 +4,8 @@ defmodule ReflectWeb.ViewHelpers do
   alias Phoenix.HTML.Form
   alias ReflectWeb.ViewHelpers
 
+  # todo gettext and i18n all the month names in here
+
   def date_select(form, field, opts \\ []) do
     Form.date_select(form, field, [builder: &ViewHelpers.date_builder/2] ++ opts)
   end
@@ -12,18 +14,27 @@ defmodule ReflectWeb.ViewHelpers do
     Form.datetime_select(form, field, [builder: &ViewHelpers.datetime_builder/1] ++ opts)
   end
 
-  def date_builder(builder, date_label \\ "") do
+  def date_builder(builder) do
     ~e"""
-      <%= date_label %> <%= builder.(:day, []) %> <%= builder.(:month, []) %> <%= builder.(:year, []) %>
+      <%= builder.(:day, []) %> <%= builder.(:month, []) %> <%= builder.(:year, []) %>
     """
   end
 
   def datetime_builder(builder) do
-    date_e = date_builder(builder, "Date: ")
+    date_e = date_builder(builder)
     time_e = ~e"""
-        Time: <%= builder.(:hour, []) %> : <%= builder.(:minute, []) %>
+        <%= builder.(:hour, []) %> : <%= builder.(:minute, []) %>
       """
 
     [date_e|time_e]
+  end
+
+  @months %{1 => "Jan", 2 => "Feb", 3 => "Mar", 4 => "Apr",
+    5 => "May", 6 => "Jun", 7 => "Jul", 8 => "Aug", 9 => "Sep",
+    10 => "Oct", 11 => "Nov", 12 => "Dec"
+  }
+  def date_render(%DateTime{} = datetime) do
+    [datetime.day, @months[datetime.month], datetime.year]
+    |> Enum.join(" ")
   end
 end
